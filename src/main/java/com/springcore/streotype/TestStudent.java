a810f0552a96c627bc1279c7e8bec96405f18eb5
admiremeforever <poddarmanish1997@gmail.com>
2023-03-07 01:18:25 +0530

    worked with singelton and prototype bean scope

--- src/main/java/com/springcore/streotype/TestStudent.java
+++ src/main/java/com/springcore/streotype/TestStudent.java
@@ -8,7 +8,18 @@ public class TestStudent {
 	public static void main(String[] args) {
 		ApplicationContext con = new ClassPathXmlApplicationContext("com/springcore/streotype/streoconfig.xml");
 		Student student = con.getBean("ob", Student.class);
-		System.out.println(student);
-		System.out.println(student.getAddress());
+		/*
+		 * System.out.println(student); System.out.println(student.getAddress());
+		 */
+		System.out.println(student.hashCode());
+
+		Student student2 = con.getBean("ob", Student.class);
+		System.out.println(student2.hashCode());
+System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+		Teacher t1 = con.getBean("teacher", Teacher.class);
+		Teacher t2 = con.getBean("teacher", Teacher.class);
+		System.out.println(t1.hashCode());
+		System.out.println(t2.hashCode());
+		
 	}
 }
